eef87cd809e6d7daf2e24a8ce2fb5d8be91725b3
yunbiyomi <yuyu5850@daum.net>
2023-05-07 23:42:20 +0900

    완성

--- src/server.js
+++ src/server.js
@@ -1,38 +1,43 @@
 import http from "http";
-// import WebSocket from "ws";
-import SocketIO from "socket.io"
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public")); // 폴더를 유저에게 공개
-app.get("/", (req, res) => res.render("home")); // 홈페이지로 이동 시 사용될 템플릿을 렌더
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-    console.log(socket);
-})
+    socket["nickname"] = "Anonymous";
 
-wsServer.on("connection", (socket) => {
-    socket.on("join_room", (roomName) => {
+    socket.on("enter_room", (roomName, done) => {
         socket.join(roomName);
+        done();
         socket.to(roomName).emit("welcome");
     });
 
-    socket.on("offer", (offer, roomName) => {
-        socket.to(roomName).emit("offer", offer);
+    socket.on("set_nickname", (nickname) => {
+        socket["nickname"] = nickname;
     });
-    socket.on("answer", (answer, roomName) => {
-        socket.to(roomName).emit("answer", answer);
-    })
-    socket.on("ice", (ice, roomName) => {
-        socket.to(roomName).emit("ice", ice);
-    })
+
+    socket.on("send_offer", (offer, roomName) => {
+        socket.to(roomName).emit("receive_offer", offer);
+    });
+
+    socket.on("send_answer", (answer, roomName) => {
+        socket.to(roomName).emit("receive_answer", answer);
+    });
+
+    socket.on("send_ice", (ice, roomName) => {
+        socket.to(roomName).emit("receive_ice", ice);
+    });
 });
 
-const handleListen = () => console.log('listening on http://localhost:3000');
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
